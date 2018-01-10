92b2739af07152f522c409ad334a1683d551b8ee
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-10 20:37:10 +0700

    xong

--- a.js
+++ a.js
@@ -4,11 +4,15 @@ const URL = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc3765573683
 
 function getTemp(cityName, fn) {
     request(URL + cityName, (err, res, body) => {
-        if (err) return console.log(err);
+        if (err) return fn(err, null);
         const obj = JSON.parse(body);
-        fn(obj.main.temp);
+        if (!obj.main) return fn(new Error('Khong tim thay thanh pho'), null);
+        fn(null, obj.main.temp);
     });
 }
 
-getTemp('Berlin', console.log);
-getTemp('Tokyo', t => console.log('Temp: ' + t));
+getTemp('ascdjahsd', (err, temp) => {
+    if (err) console.log(err.message);
+    console.log(temp);
+});
+// getTemp('Tokyo', t => console.log('Temp: ' + t));

736d7ef9015061a6a46775adb20e35c8afc5411c
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-10 19:49:49 +0700

    a.js

--- a.js
+++ a.js
@@ -1,2 +1,8 @@
+const request = require('request');
+
 const URL = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=';
 
+request(URL + 'Tokyo', (err, res, body) => {
+    if (err) return console.log(err);
+    console.log(body);
+});

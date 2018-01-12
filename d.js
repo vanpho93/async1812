a4a1228af00c389b98f6b7a0b77b3174c10d274b
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-12 20:02:33 +0700

    d.jss

---
+++ d.js
@@ -0,0 +1,39 @@
+const request = require('request');
+
+function cong(a, b) {
+    return new Promise((resolve, reject) => {
+        const url = `http://localhost:3000/tinh/CONG/${a}/${b}`;
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Type error.'));
+        request(url, (err, res, body) => {
+            if (err) return reject(err);
+            return resolve(body);
+        });
+    });
+}
+
+function nhan(a, b) {
+    return new Promise((resolve, reject) => {
+        const url = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Type error.'));
+        request(url, (err, res, body) => {
+            if (err) return reject(err);
+            return resolve(body);
+        });
+    });
+}
+
+function chia(a, b) {
+    return new Promise((resolve, reject) => {
+        const url = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Type error.'));
+        if (b == 0) return reject(new Error('Divide by zero.'));
+        request(url, (err, res, body) => {
+            if (err) return reject(err);
+            return resolve(body);
+        });
+    });
+}
+
+cong(4, 5)
+.then(result => console.log(result))
+.catch(err => console.log(err));
